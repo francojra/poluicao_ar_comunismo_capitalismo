eb263aefe0e198c293385932d7e0f16d1d4a0888
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-01 23:21:13 -0300

    Add files via upload

--- poluicao_ar_capitalismo_comunismo.R
+++ poluicao_ar_capitalismo_comunismo.R
@@ -51,4 +51,21 @@ par2 <- par %>%
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
 
+c4a("safe", 6)
 
+ggplot(par1, aes(x = fct_reorder(Entity, media), y = media, 
+                 fill = Entity)) +
+  geom_col(width = 0.9) +
+  geom_errorbar(aes(ymin = media - se, ymax = media + se),
+                size = 0.8, width = 0.2) +
+  scale_fill_manual(values = c("#88CCEE", "#CC6677", "#DDCC77",
+                              "#117733", "#332288", "#AA4499")) +
+  scale_x_discrete(labels = c("Japão", "Estados Unidos", "Alemanha",
+                               "Cuba", "China", "Coreia do Norte")) +
+  scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  labs(x = "Países", y = "Porcentagem de mortes 
+                       atribuídas a poluição do ar") +
+  theme_ipsum(axis_title_size = 16,
+              axis_text_size = 14) +
+  theme(legend.position = "none",
+        axis.text = element_text(color = "black"))
