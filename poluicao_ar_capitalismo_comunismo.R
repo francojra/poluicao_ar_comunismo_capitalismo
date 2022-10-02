4774623644d26eb3018abda376b3c8e059795c3c
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-01 22:55:25 -0300

    Add files via upload

---
+++ poluicao_ar_capitalismo_comunismo.R
@@ -0,0 +1,25 @@
+
+# Poluição do ar em países capitalistas e comunistas ---------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 01/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/air-pollution -------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Poluição do ar é um dos maiores problemas de saúde e ambietais. Ela se desenvolve
+### em dois contextos: indoor (poluição doméstica) e outdoor (poluição ao ar livre).
+
+### Nós observamos os dados em detalhes e pesquisamos sobre os impactos na saúde
+### pela poluição indoor e pela poluição outdoor, incluindo as mortes atribuídas 
+### ao problema e as causas dela em torno do mundo.
+
+### Globalmente, a poluição do ar contribuiu para 11.65% das causas de morte no
+### ano de 2019.
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(cols4all)
+library(hrbrthemes)
+
+
